426df2ca58a6fb3ce16016842230be30f629d26b
Brian Brown <brian@thekompanee.com>
2021-10-17 22:03:56 -0500

    Add navigation and header

--- themes/tmfsa-theme/app/Controllers/App.php
+++ themes/tmfsa-theme/app/Controllers/App.php
@@ -4,15 +4,13 @@ namespace App\Controllers;
 
 use Sober\Controller\Controller;
 
-class App extends Controller
-{
-    public function siteName()
-    {
+class App extends Controller {
+
+    public function siteName() {
         return get_bloginfo('name');
     }
 
-    public static function title()
-    {
+    public static function title() {
         if (is_home()) {
             if ($home = get_option('page_for_posts', true)) {
                 return get_the_title($home);
@@ -30,4 +28,39 @@ class App extends Controller
         }
         return get_the_title();
     }
+
+    public function primaryMenu() {
+        $args = array(
+            'theme_location'    => 'primary_navigation',
+            'menu_class'        => 'navbar-nav mr-auto',
+            'container'         => '',
+            'walker'            => new \App\wp_bootstrap4_navwalker(),
+        );
+        return $args;
+    }
+
+    public function featuredImg() {
+
+        $post_id = get_queried_object_id();
+
+        if ( has_post_thumbnail( $post_id ) ) {
+
+            $thumb_id        = get_post_thumbnail_id( $post_id );
+            $thumb_url_array = wp_get_attachment_image_src( $thumb_id, 'large', TRUE );
+
+            return (object) array(
+
+                'large_size'  => $thumb_url_array[0],
+                'full_size'   => wp_get_original_image_url( $thumb_id ),
+                'alt'         => get_post_meta( $thumb_id, '_wp_attachment_image_alt', TRUE ),
+                'title'       => get_the_title( $thumb_id ),
+            
+            );
+
+        } else {
+
+            return;
+       }
+    }
+
 }
